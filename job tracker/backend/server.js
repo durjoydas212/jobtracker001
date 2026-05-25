db50bbf16de65fcb5b361b99b3c93a568c621e88
Durjoy Das <durjoydas2122@gmail.com.com>
2026-05-26 01:33:37 +0530

    Bound server to process env dynamic port for Render routing

--- job tracker/backend/server.js
+++ job tracker/backend/server.js
@@ -53,5 +53,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is cleanly running on port ${PORT}`);
 });
